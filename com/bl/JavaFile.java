c2ed615a2ef457954b9f58c56f72fd2aab5086b8
saiprakash4966 <97298373+saiprakash4966@users.noreply.github.com>
2022-03-23 12:16:39 +0530

    Update JavaFile.java

--- com/bl/JavaFile.java
+++ com/bl/JavaFile.java
@@ -1,15 +1,32 @@
 package com.bl;
-
+/*Creating Javafile Class to read Person details in AddressBook
+ * 
+ */
 public class JavaFile 
 {
-	
+	/**
+	 * private variables can only be accessed within the same class (an outside
+	 * class has no access to it) private = restricted access However, it is
+	 * possible to access them if we provide public get and set methods.
+	 */
 	    static int id = JavaFileMain.people.size();
 	    private String Firstname;
 	    private String Lastname;
 	    private String phoneNumber;
 	    private String email;
 	    private String address;
-
+    
+	    /**
+		 * creating a parameterized constructor of ContactDetails by passing parameters
+		 * with no return type
+		 * 
+		 * @param firstName
+		 * @param lastName
+		 * @param address
+		 * @param phonenumber
+		 * @param email
+		 * 
+		 */
 	    JavaFile(String name, String surname, String phoneNumber, String email, String address) {
 	        this.Firstname = Firstname;
 	        this.Lastname = Lastname;
@@ -18,6 +35,10 @@ public class JavaFile
 	        this.address = address;
 	        id++;
 	    }
+	    
+	    /*Getters and Setters
+	     * 
+	     */
 
 	    String getFirstName() {
 	        return Firstname;
@@ -39,6 +60,9 @@ public class JavaFile
 	        return address;
 	    }
 
+	    /*To string for the dispaly pattern
+	     * 
+	     */
 	    @Override
 	    public String toString() {
 	        return "\n\nfirstName: " + getFirstName() + "\nlastname: " + getLastName() + "\nPhone number: " + getPhoneNumber() + "\nEmail: " +
